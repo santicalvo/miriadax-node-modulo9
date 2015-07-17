b0e871512013a7638b7c16ef5e2692d40525f66e
santi <santicalvo@gmail.com>
2015-07-17 13:41:59 +0200

    Cambio footer y autor

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -10,3 +10,7 @@ exports.answer = function (req, res){
         res.render('quizes/answer', {respuesta: 'Incorrecto'});
     }
 }
+
+exports.author = function (req, res){
+    res.render('author', {author: 'Santi'});
+}
