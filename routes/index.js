e1f9d44d3f1ba8a032b89046b4db09720868985a
santi <santicalvo@gmail.com>
2015-08-19 19:47:53 +0200

    creados archivos de autenticacion

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
-/* GET home page. */
-/*
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
-});
-*/
+var sessionController = require('../controllers/session_controller');
 
 router.get('/', function(req, res) {
     res.render('index', { title: 'Quiz', errors:[] });
@@ -17,7 +12,10 @@ router.get('/', function(req, res) {
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
 
-
+//Deficinición de las rutas de sesión:
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy);
 
 
 router.get('/quizes', quizController.index);
